020ab7b2c6537643db7251696ef830d1e0375e98
Jonas Nilsson <jonas.nilsson@esss.se>
2019-05-28 09:03:22 +0200

    Updated streaming data types.

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans.util import files
 
 class StreamingDataTypesConan(ConanFile):
     name = "streaming-data-types"
-    version = "c45d2ec"
+    version = "fb4d847"
     license = "BSD 2-Clause"
     url = "https://bintray.com/ess-dmsc/streaming-data-types"
     settings = "compiler", "arch"
