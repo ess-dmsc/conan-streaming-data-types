199b35da7164188e78465726bfa1f70f030d211f
Kenan Muric <kenan.muric@ess.eu>
2022-12-07 11:34:52 +0100

    updating commit

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conans.util import files
 
 class StreamingDataTypesConan(ConanFile):
     name = "streaming-data-types"
-    version = "e644b3c"
+    version = "5b537f9"
     license = "BSD 2-Clause"
     url = "https://bintray.com/ess-dmsc/streaming-data-types"
     settings = "compiler", "arch"
